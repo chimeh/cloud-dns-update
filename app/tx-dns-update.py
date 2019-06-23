6f44c84253be9f1a7eb587eb66ca1fe84405df46
Jimin Huang <jimin.huang@nx-engine.com>
2019-06-23 19:20:27 +0800

    出口公网ip于dns解析不同时更新

--- app/tx-dns-update.py
+++ app/tx-dns-update.py
@@ -10,6 +10,7 @@ import datetime
 import time
 import argparse
 import sys
+from socket import gethostbyname
 reload(sys) 
 sys.setdefaultencoding('utf8') 
 #get your true IP
@@ -19,7 +20,14 @@ def get_out_ip():
     response = session.get('http://ifconfig.co/ip')
     myip = response.text.strip()
     return "%s" % myip
-
+    
+def get_resolve_ip(host):
+    try:
+        resolve_ip = gethostbyname(host)
+    except Exception as e:
+        resolve_ip = "0.0.0.0"
+    finally:
+        return "%s" % resolve_ip
 
 #refer to https://github.com/QcloudApi/qcloudapi-sdk-python
 def get_cns_service(secretId,secretKey):
@@ -65,53 +73,37 @@ def modify_dns_record_ip(cns_service,domain,subdomain,record_id,ip):
     }
     print(params)
     return cns_service.call(action,params)
-def monitor_domain(my_domain, my_subdomain, my_secret_id, my_secret_key,my_ip):
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+def monitor_domain(domain, subdomain, secretId, secretKey,my_ip):
     #Need reqeust in https://console.cloud.tencent.com/capi
-    secretId = my_secret_id
-    secretKey = my_secret_key
-
+    host = "%s.%s" % (subdomain, domain)
+    
     #buy domain name in https://dnspod.cloud.tencent.com/?from=qcloudProductDns
-    domain = my_domain
-    subdomain = my_subdomain
+    
     if my_ip == "0.0.0.0":
-        ip = get_out_ip()
+        outip = get_out_ip()
     else:
-        ip = my_ip
-    print("my outbind ip: "+ip)
-    OUT_IP_FILE = './out_ip.txt'
-
-    if not os.path.exists(OUT_IP_FILE):
-        with open(OUT_IP_FILE,'w+') as f:
-            f.write(ip)
-            print("[dns_tool]:Cache IP:{} as file".format(ip))
-    else:
-        with open(OUT_IP_FILE,'r+') as f:
-            old_ip = f.read()
-            if old_ip == ip:
-                print("[dns_tool]:not need update,current IP:"+old_ip)
-
-            else:
-                f.seek(0)
-                f.truncate(0)
-                f.write(ip)
-                print("[dns_tool]:Update a IP:{},old ip: {}".format(ip,old_ip))
+        outip = my_ip
+    
+    resolve_ip = get_resolve_ip(host)
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +": host %s resolve to %s, outip %s" % (host, resolve_ip, outip))
 
     service = get_cns_service(secretId,secretKey)
     record_list = json.loads(get_record_list(service,domain))
     json.dumps(record_list)
 
-    is_ip = False
-    record_id = 0
-    for item in record_list["data"]["records"]:
-        if item["type"] == 'A' and item["name"] == subdomain:
-            is_ip = True
-            record_id = item["id"]
-            modify_dns_record_ip(service, domain, subdomain, record_id, ip)
-            print("[dns_tool]:Modify a record, id: {}".format(record_id))
-    if not is_ip:
-        add_dns_record_ip(service, domain,subdomain, ip)
-        print("[dns_tool]:Create a new ip record for domain: "+subdomain +"."+domain)
+    if resolve_ip != outip:
+        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +": update host %s to outip %s" % (host, outip))
+        record_id = 0
+        is_ip = False
+        for item in record_list["data"]["records"]:
+            if item["type"] == 'A' and item["name"] == subdomain:
+                is_ip = True
+                record_id = item["id"]
+                modify_dns_record_ip(service, domain, subdomain, record_id, outip)
+                print("[dns_tool]:Modify a record, id: {}".format(record_id))
+        if not is_ip:
+            add_dns_record_ip(service, domain,subdomain, outip)
+            print("[dns_tool]:Create a new ip record for domain: "+subdomain +"."+domain)
 
 def main():
     default_secretid = os.environ.get("SECRETID", 'YOURsecretid')
